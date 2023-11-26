4cb3a053c8cf436e97db370c08300fb9739c61d3
Sushank Gautam <sushankg4@gmail.com>
2023-11-26 19:44:52 +0530

    added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import logo from "../Assets/openai-logo.png";
 import "../Styles/Navbar.scss";
+import "../Styles/NavMediaQueries.scss"
+import SmallScreenMenu from "./SmallScreenMenu"
 import { IoIosArrowDown } from "react-icons/io";
 import { MdOutlineArrowOutward } from "react-icons/md";
 const Navbar = () => {
+
+  const HandleMenu =()=>{
+    
+  }
   return (
-    <div>
+    <div className="nav-container">
       <nav>
         <div className="logo-with-items">
           <div className="logo">
@@ -30,12 +36,16 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div>
+        <div className="nav-right">
           <ul>
             <li>Search</li>
             <li>Log in <MdOutlineArrowOutward /></li>
             <li className="try-chatgpt-btn">Try ChatGPT <MdOutlineArrowOutward /></li>
           </ul>
+        </div>
+
+        <div>
+          <p onClick={HandleMenu}>Menu</p>
         </div>
       </nav>
     </div>
---
+++ src/Components/SmallScreenMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SmallScreenMenu = () => {
+  return (
+    <div className='Small_screen_menu_container'>
+      
+    </div>
+  )
+}
+
+export default SmallScreenMenu
